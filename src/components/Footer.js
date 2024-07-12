f7a5dfc8eab22ae010d5f1fec1ad318deb0aeec8
tayo <gbadebotayo76@gmail.com>
2024-07-12 15:08:56 +0100

    fourth commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer (){
+
+    return(
+        <footer className="footer">
+            <div className="content">A product of Hardcore Biometric</div>
+        </footer>
+    )
+}
+
+export default Footer
